f5f6f9c6c85944590340b197700676672373e2cb
maximilionus <maximilionuss@gmail.com>
2020-10-27 01:23:30 +0300

    Implemented github workflows Squashed commit of the following:
    commit 6aadc5e7e2b21ccdfc22e7949e42d25a124bae1b
    Author: maximilionus <maximilionuss@gmail.com>
    Date:   Tue Oct 27 01:22:04 2020 +0300
    
        Upgrade to version 0.0.1-dev3 and CI enhance
    
    commit 87e11fa1ef2ccb295e870f9a2e565158c934f02c
    Author: maximilionus <maximilionuss@gmail.com>
    Date:   Tue Oct 27 00:29:24 2020 +0300
    
        Fixed upload repo for test CI release
    
    commit 73e2613f4ed8514b43f5890ee0c0fad0c677296c
    Author: maximilionus <maximilionuss@gmail.com>
    Date:   Tue Oct 27 00:20:19 2020 +0300
    
        Changed version to 0.0.1-dev1 and CI enhanced
        - Added release upload to CI
        - Renamed some CI things in main.yml workflow
        - Added `setuptools` to requirements.txt
    
    commit bf69e5eea307dc54499ccbb08ab9fdf811588027
    Author: maximilionus <maximilionuss@gmail.com>
    Date:   Mon Oct 26 23:31:46 2020 +0300
    
        CI implementation

--- configerz/info.py
+++ configerz/info.py
@@ -1,3 +1,3 @@
 __all__ = ["__version__", "__author__"]
-__version__ = "0.0.1"
+__version__ = "0.0.1-dev3"
 __author__ = "maximilionus"
--- setup.py
+++ setup.py
@@ -32,6 +32,11 @@ setup(
     classifiers=[
         "Development Status :: 1 - Planning",
         "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
         "Operating System :: OS Independent",
         "License :: OSI Approved :: MIT License",
         "Topic :: Software Development :: Libraries"
