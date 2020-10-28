aad5960ee5097a91014956bdebb6599e7f195a51
maximilionus <maximilionuss@gmail.com>
2020-10-28 23:43:27 +0300

    Fixed versions range in requirements.txt and added 'py3:only' tag to setup.py classifiers

--- setup.py
+++ setup.py
@@ -34,6 +34,7 @@ setup(
     classifiers=[
         "Development Status :: 1 - Planning",
         "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3 :: Only"
         "Programming Language :: Python :: 3.5",
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
